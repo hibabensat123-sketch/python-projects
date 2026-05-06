279fe8e228c337eb360d828c013810eb1cdf72b9
hibabensat123-sketch <hiba.bensat123@gmail.com>
2026-05-06 18:25:26 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,29 @@
+# Day 1 of my AI Security Engineering journey 
+
+name = "Hiba" 
+age = 26 
+first_city = "London"
+second_city = "Casablanca"
+
+print(f"My name is {name}. I am {age} years old and I live between {first_city} and {second_city}.")
+
+
+# A day of Hiba's life
+
+sport = input("Enter a type of sport: ")
+drink = input("Enter your favourite drink: ")
+place = input("Enter a place: ") 
+language = input("Enter a language: ")
+food = input("Enter a dish: ")
+show = input("Enter a show: ")
+
+story = f"""
+When {name} wakes up from her sleep, 
+she does her daily {sport} class after drinking her {drink}. 
+{name} loves morning walks around {first_city}; it makes her day better.
+After that, she heads to {place}, where she likes to sit down and study {language}.
+At night, she returns back home to make herself a nice {food}
+and relax by watching her favourite show, {show}.
+"""
+
+print(story)
